861f5246754af2cca2a198d67b9c486b8e9ce956
kanykei063129 <akjoltoikyzykanykei@gmail.com>
2023-04-04 11:58:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class Main extends JFrame {
+    public Main() {
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320,345);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        Main mw = new Main();
+    }
+}
